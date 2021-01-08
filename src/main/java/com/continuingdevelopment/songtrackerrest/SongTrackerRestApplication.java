83da57e68fd246a720c202b45adc6008efea73f2
Bill Camphire <308code@gmail.com>
2021-01-08 18:20:31 -0500

    Code generated by Spring Boot initializer

---
+++ src/main/java/com/continuingdevelopment/songtrackerrest/SongTrackerRestApplication.java
@@ -0,0 +1,13 @@
+package com.continuingdevelopment.songtrackerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongTrackerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongTrackerRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/continuingdevelopment/songtrackerrest/SongTrackerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuingdevelopment.songtrackerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongTrackerRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
